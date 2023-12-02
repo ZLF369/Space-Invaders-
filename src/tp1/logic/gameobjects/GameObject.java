6b0d7a80120a89d8cc9eb437b2b10ebeb815f270
ZLF369 <116487537+ZLF369@users.noreply.github.com>
2023-12-03 00:53:09 +0100

    LIST OF CHANGES
    -laser moves, and added laser collission with enemyobjects,
    
    but a 2nd laser cannot be shot (recheck that logic)
    -needs to complete laser logic
    -bomb logic too and displayal
    -ufo (diplayal and movement only)
    -list method added

--- src/tp1/logic/gameobjects/GameObject.java
+++ src/tp1/logic/gameobjects/GameObject.java
@@ -10,7 +10,7 @@ public abstract class GameObject implements GameItem {
 	protected Game game;
 	protected Move dir;
 
-	public GameObject(Game game, Position pos, int life) {	
+	public GameObject(Game game, Position pos, int life) {
 		this.pos = pos;
 		this.game = game;
 		this.life = life;
@@ -37,13 +37,58 @@ public abstract class GameObject implements GameItem {
 	protected abstract int getDamage();
 	protected abstract int getArmour();
 
+
+	/*public abstract void onDelete();
+	 *//*public abstract void automaticMove(){
+		boolean nowOnBorder = false;
+		GameObject[] AlienShip = get;
+		for (GameObject o: AlienShip) {
+			if(Game.isOnBorderX(a.getPosition())) {
+				nowOnBorder = true;
+				break;
+			}
+		}
+		for (Alien a: aliens) {
+			if(a instanceof DestroyerAlien da) {
+				da.moveBomb();
+				if(game.tryFiringChance()) da.enableBomb();
+			}
+		}
+
+		if(nowOnBorder) {
+			if(onBorder) {
+				// already was on border -> wait for move then reset onBorder
+				if(cyclesToMove == 0) onBorder = false;
+
+			} else {
+				// newly on border -> descend now
+				for (Alien a: aliens) {
+					a.changeDirection();
+					if(Game.isInFinalRow(a.getPosition()))
+						this.squadInFinalRow = true;
+				}
+				if(cyclesToMove == 0) cyclesToMove++;
+				onBorder = true;
+			}
+		}
+
+		if(cyclesToMove-- == 0) {
+			for (Alien a: aliens) a.automaticMove();
+			// reset cycle counter back to default
+			cyclesToMove = (level.numCyclesToMoveOneCell-1);
+		}
+*//*
+
+
+
+	}*/
 	public void computerAction() {};
-	
+
 	//TODO fill with your code
-	
+
 	@Override
 	public boolean performAttack(GameItem other) {return false;}
-	
+
 	@Override
 	public boolean receiveAttack(EnemyWeapon weapon) {return false;}
 
